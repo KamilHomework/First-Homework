8226e3cb2e18f9dae7fa4347e273b35e31e8a347
KamilHomework <114255453+KamilHomework@users.noreply.github.com>
2022-09-24 17:24:41 +0300

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main(int argc, char *argv[])
 {
- int n;
- cin >> n;
- int a[n]; 
- int t = n%2==0? n/2: (n/2)+1;
- for (int i=0; i<t;i++){
-  cin >> a[i];
-  a[n-1-i]=a[i];
- }
- for (int i=0;i<n;i++){
-  cout << a[i];
- }
+    int n;
+    cin >> n;
+    int a[n]; 
+    int t = n%2==0? n/2: (n/2)+1;
+    for (int i=0; i<t;i++){
+        cin >> a[i];
+        a[n-1-i]=a[i];
+    }
+    for (int i=0;i<n;i++){
+        cout << a[i];
+    }
 }
