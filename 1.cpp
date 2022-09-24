7ac1ce8cc873e5c951371dcd9a751e573b797566
KamilHomework <114255453+KamilHomework@users.noreply.github.com>
2022-09-24 17:23:49 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 int main(int argc, char *argv[])
 {
- int n;
- cin >> n;
- int a[n];
- vector <int> ans; 
- ans.push_back(0);
- for (int i=0; i<n;i++){
-  cin >> a[i];
-  if (a[i] > ans[ans.size()-1] )
-   ans.push_back(a[i]);
- } 
- for (int i=0; i < ans.size(); i++){
-  if (ans[i]!=0)
-   cout << ans[i] << endl;
- }
+    int n;
+    cin >> n;
+    int a[n];
+    vector <int> ans; 
+    ans.push_back(0);
+    for (int i=0; i<n;i++){
+        cin >> a[i];
+        if (a[i] > ans[ans.size()-1] )
+            ans.push_back(a[i]);
+    } 
+    for (int i=0; i < ans.size(); i++){
+        if (ans[i]!=0)
+            cout << ans[i] << " ";
+    }
 }
